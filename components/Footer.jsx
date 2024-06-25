67dc2b1493eef5e68a6807069d92b4164bc4ca65
baqaB714 <baqa4731@gmail.com>
2024-06-25 23:02:26 +0330

    after deleted the last project (ourPortfolio) - fist commit new project

---
+++ components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer id="footer" className="footer position-relative">
+
+        <div className="container">
+          <div className="copyright text-center ">
+            <p>© <span>Copyright</span> <strong className="px-1 sitename">iPortfolio</strong> <span>All Rights Reserved</span></p>
+          </div>
+          <div className="credits">
+            Designed by 
+            <a href="#">BootstrapMade</a>
+          </div>
+        </div>
+    
+      </footer>
+        );
+}
+
+export default Footer;
---
+++ components/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main class="main">
+
+            <section id="hero" class="hero section">
+
+                <img src="assets/img/hero-bg.jpg" alt=""  class="" />
+
+                <div class="container" data-aos-delay="100">
+                    <h2>Alex Smith</h2>
+                    <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer">Designer</span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
+                </div>
+
+            </section>
+
+        </main>
+    );
+}
+
+export default Main;
